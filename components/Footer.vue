023865ec54d4f3874013f4c2dc76a0aaa9dbea1f
Aurélien Roux <aureroux13@gmail.com>
2021-02-19 08:20:27 -0500

    create Footer component

---
+++ components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    ©
+    <a href="http://aroux.dev" target="_blank" rel="noopener noreferrer">
+      Aroux
+    </a>
+    |
+    {{ new Date().getFullYear() }}
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+  border: 1px solid;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+  padding: 0.7rem;
+}
+</style>
