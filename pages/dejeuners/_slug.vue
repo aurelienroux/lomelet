c51263f2c11ee5030bd19824998977ff93a7ddf2
Aurélien Roux <aureroux13@gmail.com>
2021-02-23 10:20:14 -0500

    create recipe folders slug files

---
+++ pages/dejeuners/_slug.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <RecipeHero :title="recipe.title" :picture="recipe.picture" />
+    <div class="glob-container">
+      <RecipeNutrition :nutrition="recipe.nutrition" />
+      <RecipeIngredients :ingredients="recipe.ingredients" :qty="recipe.qty" />
+      <RecipeInstructions :instructions="recipe.instructions" />
+      <RecipeOrigin :source="recipe.source" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const recipe = await $content('dejeuners', params.slug).fetch()
+
+    return { recipe }
+  },
+}
+</script>
---
+++ pages/smoothies/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <CatHero title="Smoothies" image="smoothies" />
+    <Filters @updateCategories="updateCategories" />
+    <NoRecipe v-if="noRecipes" />
+    <div v-else class="glob-container container">
+      <Recipe
+        v-for="(recipe, index) in filteredRecipes"
+        :key="index"
+        :recipe="recipe"
+        category="smoothies"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const recipes = await $content('smoothies').fetch()
+    return { recipes }
+  },
+  data() {
+    return {
+      selectedCategories: ['tout'],
+    }
+  },
+  computed: {
+    noRecipes() {
+      return this.filteredRecipes.length === 0
+    },
+    filteredRecipes() {
+      const selectedCategories = this.selectedCategories
+
+      if (selectedCategories.includes('tout')) {
+        return this.recipes
+      } else {
+        return this.recipes.filter((recipe) => {
+          let matchFilter = false
+
+          selectedCategories.forEach((category) => {
+            if (recipe.cat[category] === true) {
+              matchFilter = true
+            }
+          })
+          return matchFilter
+        })
+      }
+    },
+  },
+  methods: {
+    updateCategories(categories) {
+      this.selectedCategories = categories
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: grid;
+  gap: $padding-s;
+  grid-template-columns: repeat(auto-fill, minmax(30rem, 1fr));
+}
+</style>
